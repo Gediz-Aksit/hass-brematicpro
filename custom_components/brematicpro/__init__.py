3ccc809dbc415998ac154976de040d5864610190
gedizaksit <github@ailerontech.be>
2024-04-21 02:20:04 +0200

    Update configuration for integration.

--- custom_components/brematicpro/__init__.py
+++ custom_components/brematicpro/__init__.py
@@ -1,3 +1,23 @@
 """BrematicPro integration for Home Assistant"""
 
-from .const import VERSION
+"""Initialize the BrematicPro integration."""
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.helpers.typing import ConfigType
+
+from .const import DOMAIN
+
+async def async_setup(hass: HomeAssistant, config: ConfigType):
+    """Set up the BrematicPro component."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up BrematicPro from a config entry."""
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "switch")
+    )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Handle removal of an entry."""
+    return await hass.config_entries.async_forward_entry_unload(entry, "switch")
