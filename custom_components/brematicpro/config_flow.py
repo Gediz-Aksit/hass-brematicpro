f82bab678fbc81c4c61f9fd16e9c8f98f907f335
gedizaksit <github@ailerontech.be>
2024-04-22 12:36:51 +0200

    Update configuration for integration.

--- custom_components/brematicpro/config_flow.py
+++ custom_components/brematicpro/config_flow.py
@@ -4,7 +4,34 @@ from .const import DOMAIN, CONF_SYSTEM_CODE, CONF_CONFIG_JSON
 from .readconfigjson import read_and_transform_json
 
 class BrematicProConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    # ... other methods ...
+    """Config flow for BrematicPro."""
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
+
+    async def async_step_user(self, user_input=None):
+        """Handle a flow initiated by the user."""
+        errors = {}
+
+        if user_input is not None:
+            config_data = {
+                CONF_SYSTEM_CODE: user_input[CONF_SYSTEM_CODE],
+                CONF_CONFIG_JSON: user_input[CONF_CONFIG_JSON]
+            }
+            return self.async_create_entry(title="BrematicPro", data=config_data)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Required(CONF_SYSTEM_CODE): str,
+                vol.Required(CONF_CONFIG_JSON, default='BrematicPro.json'): str
+            }),
+            errors=errors
+        )
+
+    @staticmethod
+    def async_get_options_flow(config_entry):
+        """Link to the options flow for this integration."""
+        return BrematicProOptionsFlow(config_entry)
 
 class BrematicProOptionsFlow(config_entries.OptionsFlow):
     """Options flow for BrematicPro."""
@@ -20,7 +47,8 @@ class BrematicProOptionsFlow(config_entries.OptionsFlow):
             await self.hass.async_add_executor_job(
                 read_and_transform_json, self.hass, self.config_entry
             )
-            return self.async_create_entry(title="", data={})
+            # No need to create an entry for options, just end the flow
+            return self.async_abort(reason="configuration_reloaded")
 
         config_json_filename = self.config_entry.data.get(CONF_CONFIG_JSON, 'DefaultFilename.json')
 
