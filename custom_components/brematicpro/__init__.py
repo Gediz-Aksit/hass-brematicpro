589acca0dd3f7ec2a1adcb888ddb77a0655e15e2
gedizaksit <github@ailerontech.be>
2024-04-22 21:39:45 +0200

    Moving confing off a file.

--- custom_components/brematicpro/__init__.py
+++ custom_components/brematicpro/__init__.py
@@ -10,12 +10,21 @@ from .readconfigjson import read_and_transform_json
 
 _LOGGER = logging.getLogger(__name__)
 
+def ensure_domain_dict(hass, domain):
+    """Ensure the domain key in hass.data is a dict."""
+    if domain not in hass.data:
+        hass.data[domain] = {}
+    elif not isinstance(hass.data[domain], dict):
+        _LOGGER.error(f"Expected hass.data['{domain}'] to be a dict but found {type(hass.data[domain])}")
+        hass.data[domain] = {}  # Reset to dict if it's not
+
 async def async_setup(hass: HomeAssistant, config: ConfigType):
     """Set up the services for the BrematicPro integration."""
-    hass.data[DOMAIN] = {}
+    ensure_domain_dict(hass, DOMAIN)
 
     async def reload_json(call):
         """Service to reload JSON configuration data."""
+        ensure_domain_dict(hass, DOMAIN)
         for entry in hass.config_entries.async_entries(DOMAIN):
             devices_filename = entry.data.get(CONF_CONFIG_JSON, 'BrematicPro.json')
             rooms_filename = entry.data.get(CONF_ROOMS_JSON, 'BrematicProRooms.json')
@@ -31,14 +40,14 @@ async def async_setup(hass: HomeAssistant, config: ConfigType):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Setup from a config entry."""
-    if DOMAIN not in hass.data:
-        hass.data[DOMAIN] = {}
-        
+    ensure_domain_dict(hass, DOMAIN)
+
     devices_filename = entry.data.get(CONF_CONFIG_JSON, 'BrematicPro.json')
     rooms_filename = entry.data.get(CONF_ROOMS_JSON, 'BrematicProRooms.json')
     data = await hass.async_add_executor_job(read_and_transform_json, hass, devices_filename, rooms_filename)
     if data:
         hass.data[DOMAIN][entry.entry_id] = data
+        _LOGGER.debug("Loaded data for BrematicPro: %s", data)
     else:
         _LOGGER.error("Failed to load or transform data for BrematicPro")
         return False  # Return False to indicate the setup failed
@@ -49,11 +58,12 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Handle removal of an entry."""
+    ensure_domain_dict(hass, DOMAIN)
     unload_ok = all([
         await hass.config_entries.async_forward_entry_unload(entry, 'switch'),
         await hass.config_entries.async_forward_entry_unload(entry, 'light')
     ])
     if unload_ok:
-        if DOMAIN in hass.data and entry.entry_id in hass.data[DOMAIN]:
-            hass.data[DOMAIN].pop(entry.entry_id, None)
+        if entry.entry_id in hass.data[DOMAIN]:
+            hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
