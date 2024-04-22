f045ccb4810240978a02b2339c9c946517b22db0
gedizaksit <github@ailerontech.be>
2024-04-22 22:50:31 +0200

    Modify switch.py for the needed logic.

--- custom_components/brematicpro/switch.py
+++ custom_components/brematicpro/switch.py
@@ -5,7 +5,6 @@ from homeassistant.components.switch import SwitchEntity
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
-
 from .const import DOMAIN, CONF_INTERNAL_JSON
 
 _LOGGER = logging.getLogger(__name__)
@@ -15,7 +14,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
     json_data = entry.data.get(CONF_INTERNAL_JSON)
     if json_data:
         devices = json.loads(json_data)
-        entities = [BrematicSwitch(device, hass) for device in devices if device['type'] == 'switch']
+        entities = []
+        for device in devices:
+            if device['type'] == 'switch':
+                entity = BrematicSwitch(device, hass)
+                entities.append(entity)
         async_add_entities(entities, True)
     else:
         _LOGGER.error("No configuration data found for BrematicPro switches.")
@@ -30,6 +33,7 @@ class BrematicSwitch(SwitchEntity):
         self._is_on = False
         self._commands = device_info['commands']
         self._session = async_get_clientsession(hass)
+        self._area_id = self._find_area_id(hass, device_info['room'])
 
     @property
     def unique_id(self):
@@ -51,9 +55,22 @@ class BrematicSwitch(SwitchEntity):
         response = await self._session.post(self._commands['on'])
         if response.status == 200:
             self._is_on = True
+            self.async_write_ha_state()
+        else:
+            _LOGGER.error("Failed to turn on: %s", self._name)
 
     async def async_turn_off(self, **kwargs):
         """Turn the switch off."""
         response = await self._session.post(self._commands['off'])
         if response.status == 200:
             self._is_on = False
+            self.async_write_ha_state()
+        else:
+            _LOGGER.error("Failed to turn off: %s", self._name)
+
+    def _find_area_id(self, hass, room_name):
+        """Attempt to match the room name to an area in Home Assistant."""
+        for area in hass.config.area_registry.async_list_areas():
+            if area.name.lower() == room_name.lower():
+                return area.id
+        return None
