265bbcc002cce83265c5d14aa1e597b827777cd4
gedizaksit <github@ailerontech.be>
2024-04-22 21:17:05 +0200

    Moving confing off a file.

--- custom_components/brematicpro/__init__.py
+++ custom_components/brematicpro/__init__.py
@@ -12,9 +12,7 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass: HomeAssistant, config: ConfigType):
     """Set up the services for the BrematicPro integration."""
-    # Ensure DOMAIN data is a dictionary
-    if DOMAIN not in hass.data:
-        hass.data[DOMAIN] = {}
+    hass.data[DOMAIN] = {}
 
     async def reload_json(call):
         """Service to reload JSON configuration data."""
@@ -33,33 +31,27 @@ async def async_setup(hass: HomeAssistant, config: ConfigType):
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Setup from a config entry."""
-    # Explicitly initialize DOMAIN data dictionary if not already done
     if DOMAIN not in hass.data:
         hass.data[DOMAIN] = {}
-        
+
     devices_filename = entry.data.get(CONF_CONFIG_JSON, 'BrematicPro.json')
     rooms_filename = entry.data.get(CONF_ROOMS_JSON, 'BrematicProRooms.json')
     data = await hass.async_add_executor_job(read_and_transform_json, hass, devices_filename, rooms_filename)
     if data:
         hass.data[DOMAIN][entry.entry_id] = data
-        _LOGGER.debug("Loaded data for BrematicPro: %s", data)
     else:
         _LOGGER.error("Failed to load or transform data for BrematicPro")
-        return False  # Prevent further setup if data loading fails
+        return False  # Return False to indicate the setup failed
 
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, 'switch')
-    )
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, 'light')
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ['switch', 'light'])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Handle removal of an entry."""
-    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, 'switch')
-    unload_ok &= await hass.config_entries.async_forward_entry_unload(entry, 'light')
+    unload_ok = all([
+        await hass.config_entries.async_forward_entry_unload(entry, 'switch'),
+        await hass.config_entries.async_forward_entry_unload(entry, 'light')
+    ])
     if unload_ok:
-        if DOMAIN in hass.data and entry.entry_id in hass.data[DOMAIN]:
-            hass.data[DOMAIN].pop(entry.entry_id)
+        hass.data[DOMAIN].pop(entry.entry_id, None)
     return unload_ok
