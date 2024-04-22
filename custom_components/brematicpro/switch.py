9a026f8f1e4f7a913aab5bce7386f8d9276dd426
gedizaksit <github@ailerontech.be>
2024-04-23 01:03:22 +0200

    Modify readconfigjson.py.py for the needed logic.

--- custom_components/brematicpro/switch.py
+++ custom_components/brematicpro/switch.py
@@ -20,14 +20,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
     json_data = entry.data.get(CONF_INTERNAL_JSON)
     if json_data:
         devices = json.loads(json_data)
-        area_registry = ar.async_get(hass)
+        area_registry = ar.async_get(hass)  # Although fetched, it's not used here now.
         entities = []
         for device in devices:
             if device['type'] == 'switch':
-                entity = BrematicSwitch(device, hass, area_registry)
+                entity = BrematicSwitch(device, hass)
                 entities.append(entity)
         async_add_entities(entities, True)
-        hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entities  # Store references to entities
+        hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entities
         return True
     else:
         _LOGGER.error("No configuration data found for BrematicPro switches.")
@@ -36,14 +36,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
 class BrematicSwitch(SwitchEntity):
     """Representation of a BrematicPro Switch."""
 
-    def __init__(self, device_info, hass, area_registry):
+    def __init__(self, device_info, hass):
         """Initialize the switch."""
         self._unique_id = device_info['uniqueid']
         self._name = device_info['name']
         self._is_on = False
         self._commands = device_info['commands']
         self._session = async_get_clientsession(hass)
-        self.area_id = find_area_id(hass, device_info.get('room'), area_registry)
+        self.area_id = find_area_id(hass, device_info.get('room'))
 
     @property
     def unique_id(self):
@@ -75,4 +75,3 @@ class BrematicSwitch(SwitchEntity):
         if response.status == 200:
             self._is_on = False
             self.async_write_ha_state()
-
