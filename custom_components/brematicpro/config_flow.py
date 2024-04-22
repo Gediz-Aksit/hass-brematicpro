bb5f37a291c3435ddda0d9aa093ce9c0df073cac
gedizaksit <github@ailerontech.be>
2024-04-22 20:39:09 +0200

    Moving confing off a file.

--- custom_components/brematicpro/config_flow.py
+++ custom_components/brematicpro/config_flow.py
@@ -18,13 +18,10 @@ class BrematicProConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required(CONF_SYSTEM_CODE, description={"suggested_value": "Enter system code"}): str,
-                vol.Required(CONF_CONFIG_JSON, description={"suggested_value": "BrematicPro.json"}): str
+                vol.Required(CONF_SYSTEM_CODE, default='Enter your system code here'): str,
+                vol.Required(CONF_CONFIG_JSON, default='BrematicPro.json'): str,
+                vol.Required(CONF_ROOMS_JSON, default='BrematicProRooms.json'): str
             }),
-            description_placeholders={
-                'system_code_description': 'Please enter the system code for your BrematicPro device.',
-                'config_json_description': f'Enter the configuration file name. Default is "BrematicPro.json".'
-            },
             errors=errors
         )
 
@@ -41,19 +38,14 @@ class BrematicProOptionsFlow(config_entries.OptionsFlow):
     async def async_step_init(self, user_input=None):
         """Manage the options."""
         errors = {}
-        if user_input is not None and user_input.get('reload', False):
-            await self.hass.async_add_executor_job(
-                read_and_transform_json, self.hass, self.config_entry
-            )
-            return self.async_create_entry(title="", data={})
-
-        config_json_filename = self.config_entry.data.get(CONF_CONFIG_JSON, 'DefaultFilename.json')
+        if user_input is not None:
+            # Possibly reconfigure options
+            return self.async_create_entry(title="", data=user_input)
 
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
                 vol.Optional('reload', default=False): bool
             }),
-            description_placeholders={"filename": f"Current configuration file: {config_json_filename}. Reload configuration now?"},
             errors=errors
         )
