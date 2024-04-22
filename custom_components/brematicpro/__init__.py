f160253e2ff36c9996b11be7554dea281bff3220
gedizaksit <github@ailerontech.be>
2024-04-22 18:11:18 +0200

    Add functionality to read json file for rooms.

--- custom_components/brematicpro/__init__.py
+++ custom_components/brematicpro/__init__.py
@@ -12,27 +12,31 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup(hass: HomeAssistant, config: ConfigType):
     """Set up the services for the BrematicPro integration."""
+    hass.data[DOMAIN] = {}
 
     async def reload_json(call):
         """Service to reload JSON configuration data."""
         for entry in hass.config_entries.async_entries(DOMAIN):
-            data = await hass.async_add_executor_job(read_and_transform_json, hass, entry.data)
+            devices_filename = entry.data.get(CONF_CONFIG_JSON, 'BrematicPro.json')
+            rooms_filename = entry.data.get(CONF_ROOMS_JSON, 'BrematicProRooms.json')
+            data = await hass.async_add_executor_job(read_and_transform_json, hass, devices_filename, rooms_filename)
             if data:
-                hass.data[DOMAIN][entry.entry_id] = data
+                save_data_to_file(hass, data, 'BrematicProDevices.json')
                 _LOGGER.info("BrematicPro data reloaded successfully")
             else:
                 _LOGGER.error("Failed to reload BrematicPro data")
 
     hass.services.async_register(DOMAIN, "reload_json", reload_json)
-
     return True
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Setup from a config entry."""
     devices_filename = entry.data.get(CONF_CONFIG_JSON, 'BrematicPro.json')
     rooms_filename = entry.data.get(CONF_ROOMS_JSON, 'BrematicProRooms.json')
 	
     data = await hass.async_add_executor_job(read_and_transform_json, hass, devices_filename, rooms_filename)
     if data:
+        save_data_to_file(hass, data, 'BrematicProDevices.json')
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, 'switch')
         )
--- custom_components/brematicpro/readconfigjson.py
+++ custom_components/brematicpro/readconfigjson.py
@@ -1,51 +1,41 @@
 import json
 import logging
+from homeassistant.helpers.json import save_json
 
 _LOGGER = logging.getLogger(__name__)
 
-def read_and_transform_json(hass, devices_filename='BrematicPro.json', rooms_filename='BrematicProRooms.json'):
+def read_and_transform_json(hass, devices_filename, rooms_filename):
     devices_json_path = hass.config.path(devices_filename)
     rooms_json_path = hass.config.path(rooms_filename)
-
     try:
         with open(rooms_json_path, 'r') as file:
             rooms = json.load(file)
         with open(devices_json_path, 'r') as file:
             devices = json.load(file)
             transformed_data = []
-
             for item in devices.values():
                 device_name = item['name']
-                room_name = 'Unknown'  # Default if no room is found
-
-                # Extract the room from the device name
+                room_name = 'Unknown'
                 for room in rooms:
                     if room in device_name:
                         room_name = room
                         device_name = device_name.replace(room, '').strip()
-
-                # Assuming 'sys' field maps directly to frequency
                 freq = 868 if item['sys'] == 'B8' else 433 if item['sys'] == 'B4' else 0
-
-                # Augment commands with the local URL
                 commands = {cmd: item['local'] + item['commands'][cmd]['url'] for cmd in item['commands']}
-
                 transformed_data.append({
-                    "uniqueid": item.get('address', 'NoID'),  # Default 'NoID' if no address is provided
+                    "uniqueid": item.get('address', 'NoID'),
                     "name": device_name,
                     "room": room_name,
                     "freq": freq,
                     "type": item['type'],
                     "commands": commands
                 })
-
             return transformed_data
-
-    except FileNotFoundError as e:
-        _LOGGER.error(f"File not found: {e}")
-    except json.JSONDecodeError:
-        _LOGGER.error("Error decoding JSON.")
     except Exception as e:
-        _LOGGER.error(f"An error occurred: {e}")
+        _LOGGER.error(f"Error processing JSON files: {str(e)}")
+        return None
 
-    return None
+def save_data_to_file(hass, data, filename):
+    """Save transformed data to a JSON file."""
+    file_path = hass.config.path(filename)
+    save_json(file_path, data)
