3d8ae64e99c1009ce661a984f801c46db9506fe7
Gediz-Aksit <73635789+Gediz-Aksit@users.noreply.github.com>
2024-04-23 20:34:30 +0200

    Revert "Revert "Update configuration for integration.""
    This reverts commit da5fa6c2ceecf8891837c100ed0c2ee60dd46eb8.

--- custom_components/brematicpro/config_flow.py
+++ custom_components/brematicpro/config_flow.py
@@ -32,8 +32,8 @@ class BrematicProConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             if 'process_data' in user_input and user_input['process_data']:
                 await setup_entry_components(self.hass, entry)
 
-            if not errors:
-                return self.async_create_entry(title="BrematicPro", data=user_input)
+            #if not errors:
+            #    return self.async_create_entry(title="BrematicPro", data=user_input)
 
         return self.async_show_form(
             step_id="user" if self.context.get("entry_id") else "init",
