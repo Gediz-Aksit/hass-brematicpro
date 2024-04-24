e4322ba39daa698848332fb91ba6b5a465dc745f
Gediz-Aksit <73635789+Gediz-Aksit@users.noreply.github.com>
2024-04-24 02:13:09 +0200

    Update configuration for integration.

--- custom_components/brematicpro/config_flow.py
+++ custom_components/brematicpro/config_flow.py
@@ -62,4 +62,4 @@ class BrematicProOptionsFlow(config_entries.OptionsFlow):
 
     async def async_step_init(self, user_input=None):
         """Manage the options."""
-        return await BrematicProConfigFlow._common_flow_handler(self.hass, user_input)
+        return await self._common_flow_handler(user_input)
