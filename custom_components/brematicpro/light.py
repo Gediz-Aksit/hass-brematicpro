9dbfc7e47410ec562fd892156f225edfed3f6fdc
Gediz-Aksit <73635789+Gediz-Aksit@users.noreply.github.com>
2024-04-27 13:13:43 +0200

    Update configuration for integration.

--- custom_components/brematicpro/light.py
+++ custom_components/brematicpro/light.py
@@ -32,7 +32,7 @@ async def async_setup_entry(hass, entry, async_add_entities):
                     entities.append(entity)
         _LOGGER.warning('End of loop')
         async_add_entities(entities, True)
-		hass.data.setdefault(DOMAIN, {})[entry.entry_id] = list(existing_entities.values()) + entities
+        hass.data.setdefault(DOMAIN, {})[entry.entry_id] = list(existing_entities.values()) + entities
         return True
     return False
 
