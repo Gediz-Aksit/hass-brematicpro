ef74a7f25628053a07598e0948d4172fc1e4281d
Gediz-Aksit <73635789+Gediz-Aksit@users.noreply.github.com>
2024-04-27 16:54:41 +0200

    Update switch.py, clean light.py.

--- custom_components/brematicpro/light.py
+++ custom_components/brematicpro/light.py
@@ -18,23 +18,17 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
     if json_data:
         devices = json.loads(json_data)
         entities = []
-        _LOGGER.warning('Devices ' + json_data)
         existing_entities = {entity.unique_id: entity for entity in hass.data.get(DOMAIN, {}).get(entry.entry_id, [])}
         for device in devices:
             if device['type'] == 'light':
-                _LOGGER.warning('Type ' + device['type'])
                 unique_id = device['uniqueid']
-                _LOGGER.warning('Unique ID ' + unique_id)
                 #area_id = find_area_id(hass, device.get('room'))
                 if unique_id in existing_entities:
-                    _LOGGER.warning('Existing')
                     entity = existing_entities[unique_id]
                     entity.update_device(device)
                 else:
-                    _LOGGER.warning('New')
                     entity = BrematicProLight(device, hass)
                     entities.append(entity)
-        _LOGGER.warning('End of loop')
         async_add_entities(entities, True)
         #hass.data[DOMAIN][entry.entry_id] = list(existing_entities.values()) + entities
         hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entities
@@ -46,14 +40,12 @@ class BrematicProLight(LightEntity):
 
     def __init__(self, device, hass):
         """Initialize the light."""
-        _LOGGER.warning('Adding ' + device["name"])
         #self._device = device
         self._unique_id = device['uniqueid']
         self._name = device["name"]
         self._is_on = False
         self._commands = device['commands']
         self._session = async_get_clientsession(hass)
-        _LOGGER.warning('Added ' + device["name"])
 
     @property
     def unique_id(self):
--- custom_components/brematicpro/switch.py
+++ custom_components/brematicpro/switch.py
@@ -6,42 +6,45 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .const import DOMAIN, CONF_INTERNAL_JSON
-from .readconfigjson import find_area_id  # Ensure correct import
+from .readconfigjson import find_area_id, send_command
 
 _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     """Set up BrematicPro switches from a config entry."""
-    # Prevent setup from running more than once per entry
-    if entry.entry_id in hass.data.get(DOMAIN, {}):
-        return False
-
+	
     json_data = entry.data.get(CONF_INTERNAL_JSON)
     if json_data:
         devices = json.loads(json_data)
         entities = []
+        existing_entities = {entity.unique_id: entity for entity in hass.data.get(DOMAIN, {}).get(entry.entry_id, [])}
         for device in devices:
             if device['type'] == 'switch':
-                entity = BrematicProSwitch(device, hass)
-                entities.append(entity)
+                unique_id = device['uniqueid']
+                #area_id = find_area_id(hass, device.get('room'))
+                if unique_id in existing_entities:
+                    entity = existing_entities[unique_id]
+                    entity.update_device(device)
+                else:
+                    entity = BrematicProSwitch(device, hass)
+                    entities.append(entity)
         async_add_entities(entities, True)
+        #hass.data[DOMAIN][entry.entry_id] = list(existing_entities.values()) + entities
         hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entities
         return True
-    else:
-        _LOGGER.error("No configuration data found for BrematicPro switches.")
-        return False
+    return False
 
 class BrematicProSwitch(SwitchEntity):
     """Representation of a BrematicPro Switch."""
 
-    def __init__(self, device_info, hass):
+    def __init__(self, device, hass):
         """Initialize the switch."""
-        self._unique_id = device_info['uniqueid']
-        self._name = device_info['name']
+        #self._device = device
+        self._unique_id = device['uniqueid']
+        self._name = device["name"]
         self._is_on = False
-        self._commands = device_info['commands']
+        self._commands = device['commands']
         self._session = async_get_clientsession(hass)
-        self.area_id = find_area_id(hass, device_info.get('room'))
 
     @property
     def unique_id(self):
@@ -59,17 +62,16 @@ class BrematicProSwitch(SwitchEntity):
         return self._is_on
 
     async def async_turn_on(self, **kwargs):
-        """Turn the switch on."""
-        response = await self._session.post(self._commands['on'])
-        _LOGGER.info(f"Turn on response: {response.status} - {await response.text()}")
-        if response.status == 200:
+        """Instruct the switch on."""
+        response_status = await send_command(self._commands["on"])
+        if response_status == 200:
             self._is_on = True
             self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
-        """Turn the switch off."""
-        response = await self._session.post(self._commands['off'])
-        _LOGGER.info(f"Turn off response: {response.status} - {await response.text()}")
-        if response.status == 200:
+        """Instruct the switch off."""
+        response_status = await send_command(self._commands["off"])
+        if response_status == 200:
             self._is_on = False
             self.async_write_ha_state()
+
