44f2bf329062c52f62e0878f1e9e23819579e864
Gediz-Aksit <73635789+Gediz-Aksit@users.noreply.github.com>
2024-05-04 18:55:23 +0200

    Fetch sensor data from BrematicPro gateway.

--- custom_components/brematicpro/BrematicProShared.py
+++ custom_components/brematicpro/BrematicProShared.py
@@ -162,9 +162,9 @@ def read_and_transform_json(hass: HomeAssistant, entry, config_json, rooms_json,
 async def setup_entry_components(hass: HomeAssistant, entry):
     """Setup entry components for BrematicPro devices."""
     await hass.config_entries.async_forward_entry_setup(entry, 'switch')
-    #await hass.config_entries.async_forward_entry_setup(entry, 'smartswitch')
+    await hass.config_entries.async_forward_entry_setup(entry, 'smartswitch')
     await hass.config_entries.async_forward_entry_setup(entry, 'light')
-    await hass.config_entries.async_forward_entry_setup(entry, 'door')
+    #await hass.config_entries.async_forward_entry_setup(entry, 'door')
     #await hass.config_entries.async_forward_entry_setup(entry, 'window')
 
 async def unload_entry_components(hass: HomeAssistant, entry):
--- custom_components/brematicpro/smartswitch.py
+++ custom_components/brematicpro/smartswitch.py
@@ -2,6 +2,7 @@ import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 
+
 from .switch import BrematicProSwitch
 from .BrematicProShared import async_common_setup_entry
 
@@ -9,13 +10,12 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     """Set up BrematicPro device from a config entry."""
-    _LOGGER.debug("Setting up smartswitch platform for BrematicPro")
     return await async_common_setup_entry(hass, entry, async_add_entities, BrematicProMeteredSwitch)
 
 class BrematicProMeteredSwitch(BrematicProSwitch):
+    """Representation of a Brematic Metered Switch."""
     _type = 'smartswitch'
     
-    """Representation of a Brematic Metered Switch."""
     def __init__(self, device, hass):
         """Initialize the smart/metered switch."""
         super().__init__(device, hass)
