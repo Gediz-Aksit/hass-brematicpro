fa63336bc275f7519f05f40b3a3ed000063378ed
Gediz-Aksit <73635789+Gediz-Aksit@users.noreply.github.com>
2024-05-05 19:45:25 +0200

    Trying to setup SmartSwitch.

--- custom_components/brematicpro/smartswitch.py
+++ custom_components/brematicpro/smartswitch.py
@@ -1,28 +1,22 @@
 import logging
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
-
+from homeassistant.components.light import LightEntity, COLOR_MODE_ONOFF
 
 from .switch import BrematicProSwitch
 from .BrematicProShared import async_common_setup_entry
 
 _LOGGER = logging.getLogger(__name__)
-_LOGGER.debug("Top-level debug statement in smartswitch.py")
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities):
     """Set up BrematicPro device from a config entry."""
-    _LOGGER.debug("async_setup_entry called in smartswitch.py")
-    return await async_common_setup_entry(hass, entry, async_add_entities, BrematicProMeteredSwitch)
+    return await async_common_setup_entry(hass, entry, async_add_entities, BrematicProLight2)
 
-class BrematicProMeteredSwitch(BrematicProSwitch):
-    """Representation of a Brematic Metered Switch."""
+class BrematicProLight2(BrematicProSwitch, LightEntity):
+    """Representation of a Brematic Light."""
     _type = 'smartswitch'
-    
+
     def __init__(self, device, hass):
-        """Initialize the smart/metered switch."""
-        _LOGGER.debug("Class smartswitch __init__")
+        """Initialize the light."""
         super().__init__(device, hass)
-        self._watt = 0.0
-        self._voltage = 0.0
-        self._kWh = 0.0
-        self._Wh = 0.0
+        self._color_mode = COLOR_MODE_ONOFF
