10658af0f9c862ff0c25ab91c0bc3de740da3eb5
Gediz-Aksit <73635789+Gediz-Aksit@users.noreply.github.com>
2024-05-12 22:51:21 +0200

    Battery status implementation.

--- custom_components/brematicpro/binary_sensor.py
+++ custom_components/brematicpro/binary_sensor.py
@@ -36,10 +36,12 @@ class BrematicProBattery(BrematicProDevice, BinarySensorEntity):
         if device_state:
             if device_state['state'][-3] == '0':
                 self._attr_is_on  = False
+            elif device_state['state'][-3] == '1':
+                self._attr_is_on  = True
             elif device_state['state'][-3] == '4':
                 self._attr_is_on  = None
             else:
-                self._attr_is_on  = True
+                self._attr_is_on  = None
         else:
             self._attr_is_on  = None
 
