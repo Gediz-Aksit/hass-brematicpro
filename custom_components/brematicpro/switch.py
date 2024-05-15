94490c548e04dfb39ade19023d4334ad1d824016
Gediz-Aksit <73635789+Gediz-Aksit@users.noreply.github.com>
2024-05-15 11:12:19 +0200

    Implement siren entity.

--- custom_components/brematicpro/switch.py
+++ custom_components/brematicpro/switch.py
@@ -17,7 +17,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_e
     return await async_common_setup_entry(hass, entry, async_add_entities, BrematicProSwitch) and \
            await async_common_setup_entry(hass, entry, async_add_entities, BrematicProMeteredSwitch)
 
-class BrematicProSwitch(SwitchEntity, BrematicProEntityWithCommands):
+class BrematicProSwitch(BrematicProEntityWithCommands, SwitchEntity):
     """Representation of a BrematicPro Switch."""
     _type = 'switch'
 
